1be37409fb61917221e7cbb5cdfc7214de74d744
wonhwipark <wonhwipark02@gmail.com>
2025-07-03 19:40:00 +0900

    test

--- Project_RestaurantBooking/test.cpp
+++ Project_RestaurantBooking/test.cpp
@@ -1,6 +1,40 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 
+
+using namespace testing;
+
+class BookingScheduleFixture : public Test {
+public:
+	tm makeTime(int year, int month, int day, int hour, int minute) {
+		tm time = { 0, minute, hour, day, month - 1, year - 1900, 0, 0, -1 };
+		mktime(&time); // 나머지 tm 구조체 맴버값에 대해 자동으로 계산하여 채움
+		return time;
+	}
+
+	static constexpr int UNDER_CAPACITY = 1;
+	static constexpr int CAPACITY_PER_HOUR = 3;
+	tm NOT_ON_THE_HOUR;
+	Customer customer{ "Fake name", "010-1234-5678" };
+
+protected:
+	void SetUp() override {
+		NOT_ON_THE_HOUR = makeTime(2021, 3, 26, 9, 0); // 2021년 3월 26일 9시 정각
+	}
+};
+
+TEST_F(BookingScheduleFixture, 예약은정시에만가능하다정시가아닌경우예약불가) {
+	//arrange
+	Schedule* schedule = new Schedule{ NOT_ON_THE_HOUR, 1, customer };
+	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+	//act
+	EXPECT_THROW({
+	bookingScheduler.addSchedule(schedule);
+		}, std::runtime_error);
+	//assert
+	//expected runtime exception
+}
+
 TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
 
 }
